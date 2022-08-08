268285b22d54dcbf5834604fe81d28aa8687f2c6
raedaw <raedawlaw@gmail.com>
2022-08-08 13:01:45 +0100

    add login component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import "./App.css";
+import "./Styles/App.css";
 import { getDatabase } from "firebase/database";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from "axios";
+import Login from "./components/Login";
 
 function App() {
-  axios
-    .get(
-      "https://just-friends-4c0f0-default-rtdb.europe-west1.firebasedatabase.app/.json"
-    )
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  // axios
+  //   .get(
+  //     "https://just-friends-4c0f0-default-rtdb.europe-west1.firebasedatabase.app/.json"
+  //   )
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+
   return (
-    <div className="App">
-      <p>hello</p>
+    <div className="app">
+      <h1>Hello</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
