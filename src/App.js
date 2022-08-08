b852f3bc0dbb7efcbb5bfef406a4775bf45fe0cb
raedaw <raedawlaw@gmail.com>
2022-08-08 12:00:20 +0100

    set up firebase with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getDatabase } from "firebase/database";
+import axios from "axios";
 
 function App() {
+  axios
+    .get(
+      "https://just-friends-4c0f0-default-rtdb.europe-west1.firebasedatabase.app/.json"
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello</p>
     </div>
   );
 }
