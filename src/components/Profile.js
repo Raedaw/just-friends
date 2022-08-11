e533f123dd2e1223b7b792d24d112629f06acc54
raedaw <raedawlaw@gmail.com>
2022-08-11 11:05:21 +0100

    handle form validation for bio and avatar url

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,36 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { setProfile } from "../utils/firebase";
+import { imageRegex } from "../utils/imageRegex";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  bio: yup.string().min(10).required(),
+  avatarURL: yup
+    .string()
+    .matches(imageRegex, "Please enter a valid image url")
+    .required(),
+});
 
 const Profile = () => {
   const navigate = useNavigate();
   const [profileInfo, setProfileInfo] = useState({});
   const [submitInfo, setSubmitInfo] = useState({});
   const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    // if (!profileInfo.bio || !profileInfo.avatar) {
-    //   const error = "You must submit an avatar URL & bio";
-    // }
-    setSubmitInfo(profileInfo);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    setProfileInfo(data);
+    setSubmitInfo(data);
   };
-
-  const changeHandler = (event) => {
-    const newObj = { ...profileInfo };
-    if (event.target.id === "bio") {
-      newObj.bio = event.target.value;
-    } else {
-      newObj.avatarURL = event.target.value;
-    }
-    setProfileInfo(newObj);
-  };
-
-  useEffect(() => {
-    if (profileInfo.bio && profileInfo.avatarURL) {
-      setButtonDisabled(false);
-    }
-  }, [profileInfo]);
 
   useEffect(() => {
     if (submitInfo.bio) {
@@ -43,30 +45,17 @@ const Profile = () => {
   return (
     <div className="selectArea">
       <h2>Edit Profile:</h2>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="bio">Write Bio:</label>
-        <textarea
-          id="bio"
-          onChange={(event) => {
-            changeHandler(event);
-          }}
-        ></textarea>
+        <textarea id="bio" {...register("bio")}></textarea>
+        <p>{errors.bio?.message}</p>
         <label htmlFor="avatar">Avatar URL:</label>
-        <input
-          id="avatar"
-          onChange={(event) => {
-            changeHandler(event);
-          }}
-        ></input>
-        <button className="submit" disabled={buttonDisabled}>
-          Submit
-        </button>
+        <input id="avatar" {...register("avatarURL")}></input>
+        <p>{errors.avatarURL?.message}</p>
+        <input type="submit" className="submit" />
       </form>
     </div>
   );
 };
 
 export default Profile;
-
-// TODO - Regex on the URL?
-// TODO - Error Handling
